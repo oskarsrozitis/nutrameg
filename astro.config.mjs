0fc1cb2f2db746f7e2a903f5234c7b8e4c76fd96
BCOskarsRozitis <oskars.rozitis@gmail.com>
2024-07-17 11:24:15 +0300

    astro.config.mjs fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import relativeLinks from "astro-relative-links";
 
 // https://astro.build/config
