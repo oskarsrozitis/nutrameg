90bf704b8c1b2653b5887f56e293a8fb52526ce2
BCOskarsRozitis <oskars.rozitis@gmail.com>
2024-07-17 17:16:54 +0300

    refactor of layout element

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontSize: {
+			sm: '0.8rem',
+			base: '1rem',
+			xl: '1.4rem',
+			'2xl': '1.8rem',
+			'3xl': '2.2rem',
+			'4xl': '2.6rem',
+			'5xl': '3rem',
+		},
 		extend: {
 			fontFamily: {
 				serif: ['"Young Serif"', 'serif'],
@@ -9,6 +18,10 @@ export default {
 			boxShadow: {
 				'inner': 'inset 0px 10px 10px 0px rgba(81, 135, 109, 1)',
 			},
+			lineHeight: {
+				'extra-loose': '2.5',
+				'12': '4rem',
+			}
 		},
 	},
 	plugins: [
